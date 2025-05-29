c0c0b2b18b75f1b7c524aca696fc73ea33872581
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-29 10:10:10 +0100

    Update VideoBackground.jsVideoBackground.js

--- src/components/VideoBackground.jsVideoBackground.js
+++ src/components/VideoBackground.jsVideoBackground.js
@@ -5,7 +5,7 @@ export default function VideoBackground() {
   return (
     <div className="video-container">
       <video autoPlay loop muted>
-        <source src="https://example.com/demo-video.mp4" type="video/mp4" />
+        <source src="https://player.cloudinary.com/embed/?cloud_name=ddtjih2yl&public_id=samples%2Fsea-turtle&profile=cld-default" type="video/mp4" />
         Your browser doesn’t support videos.
       </video>
       <div className="video-overlay">

1a406e060c07836c12d62b40c391c70a0355b427
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-29 10:20:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,3 +15,21 @@ function App() {
 }
 
 export default App;
+
+import React from 'react';
+  import './App.css';
+  import VideoBackground from './components/VideoBackground';
+  import MembershipButtons from './components/MembershipButtons';
+  import Navbar from './components/Navbar';
+
+  function App() {
+    return (
+      <div className="app">
+        <Navbar />
+        <VideoBackground />
+        <MembershipButtons />
+      </div>
+    );
+  }
+
+  export default App;
